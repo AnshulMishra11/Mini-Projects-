a19f4cddbbf530b8b9745716ce6ff9abab308362
AnshulMishra11 <111335247+AnshulMishra11@users.noreply.github.com>
2023-02-20 21:09:33 +0530

    Add files via upload

---
+++ Social Media Blocker Extension/data.js
@@ -0,0 +1,35 @@
+
+
+switch (window.location.hostname) {
+  case "www.youtube.com":
+   document.body.innerHTML = "Youtube Not Reachable" ;
+    break;
+  case "www.facebook.com":
+    document.body.innerHTML = "Facebook  Not Reachable" ;
+    break;
+  case "www.netflix.com":
+    document.body.innerHTML = "Netflix  Not Reachable";
+    break;
+
+  case "www.snapchat.com":
+  document.body.innerHTML = "snapchat  Not Reachable";
+  break;
+
+  case "www.instagram.com":
+  document.body.innerHTML = "instagram  Not Reachable";
+  break;
+
+  case "www.roblox.com":
+    document.body.innerHTML = "roblox  Not Reachable";
+    break;
+  case "discord.com":
+    document.body.innerHTML = "discord  Not Reachable";
+    break;
+  case "www.spotify.com":
+    document.body.innerHTML = "Spotify  Not Reachable";
+    break;
+
+  case "www.telegram.org":
+    document.body.innerHTML = "Telegram  Not Reachable";
+    break;
+}
